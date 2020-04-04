16885317d002e4eb101a8632a0accde89d3b0e9a
ArJun <iloveyou138@vip.qq.com>
2020-04-04 21:11:44 +0800

    update routers

---
+++ routers/router.go
@@ -0,0 +1,34 @@
+package routers
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type WebRouter struct {
+	Name 		string
+	Method 		string
+	Pattern 	string
+	HandlerFunc http.HandlerFunc
+}
+
+// 声明webRouter切片存放所有web路由
+type WebRouters []WebRouter
+
+// 定义所有web路由
+var webRouters = WebRouters{
+
+}
+
+func NewRouter() *mux.Router  {
+	// 创建Mux.router路由
+	router := mux.NewRouter().StrictSlash(true)
+	// 遍历web.go中定义的所有 webRouters
+	for _, route := range webRouters {
+		router.Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			HandlerFunc(route.HandlerFunc)
+	}
+	return router
+}
